058860979b1bf806575e7f9f3933d67d5ec4a3ed
tony51307 <tony51307@tony51307deMBP2.attlocal.net>
2023-06-17 23:00:27 -0700

    change

--- app.py
+++ app.py
@@ -1,39 +1,70 @@
+import streamlit as st 
 import os
-from utils import get_chatbot, generate_visual_code, extract_python_code
 import pandas as pd
-from dash import Dash, dcc, html, Input, Output, State, callback
-
-
-os.environ['OPENAI_API_KEY'] = ''
-
-chat = get_chatbot()
-
-# demo data
-data = pd.DataFrame({'date':['2022/01/01','2022/01/02','2022/01/03','2022/01/04','2022/01/05'],
-                     'price':[100,200,50,30,500]})
-
-app = Dash(__name__)
-
-app.layout = html.Div([
-    html.H1(children='AI Visualizer', style={'textAlign':'center'}),
-    html.Div( dcc.Textarea(id='input-on-submit',style={'width': '50%', 'height': 100, 'align':'center'}),
-            style={'align':'center'}),
-    html.Div(html.Button('Submit', id='submit-val', n_clicks=0),style={'align':'center'}),
-    dcc.Graph(id='graph-content')
-])
-
-@callback(
-    Output('graph-content', 'figure'),
-    Input('submit-val', 'n_clicks'),
-    State('input-on-submit', 'value')
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
 )
-def update_output(n_clicks, value):
-    if n_clicks>0:
-        res = generate_visual_code(data, value, chat)
-        code = extract_python_code(res.content)
-        exec(code, globals()) # the code from LLM will store the graph in the variable "fig"
-        return fig
-    return {}
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
+from langchain.agents import create_pandas_dataframe_agent
+from pandasai import PandasAI
+from utils import *
+from langchain.chat_models import ChatOpenAI
+from pandasai.llm.openai import OpenAI
 
-if __name__ == '__main__':
-    app.run_server(debug=False)
+os.environ['OPENAI_API_KEY'] = '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'
+chat = get_chatbot(model_name='gpt-4-0613')
+openai_api_key = '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'
+#data_analyze_agent = create_pandas_dataframe_agent(chat, data, memory=ConversationBufferMemory())
+llm = OpenAI(api_token=openai_api_key)
+pandas_ai = PandasAI(llm)
+fig = None
+
+st.set_page_config(layout='wide')
+st.title("AI Visualizer - AI Data Visualization and Analysis")
+
+openai_api_key = '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'
+input_csv = st.file_uploader("Upload your CSV file here", type=['csv'])
+
+if input_csv is not None:
+    col1, col2 = st.columns([1, 1])
+
+    with col1:
+        st.info("CSV Uploaded Successfully")
+        data = pd.read_csv(input_csv)
+        st.dataframe(data, use_container_width=True)
+
+    with col2:
+        st.info("Chat Below")
+        input_text = st.text_area("Enter your query")
+
+        if input_text is not None:
+            if st.button("Done"):
+                st.info("Your Query: " + input_text)
+                
+                purpose = detect_qa_or_plot(input_text, chat)
+                
+                if purpose == 'Plotting':
+                    response = generate_visual_code(data, input_text, chat)
+                    code = extract_python_code(response)
+                    print(code)
+                    try:
+                        exec(code.replace('fig.show()',''))
+                        st.success('AI Response: Ok let me plot for you! Expand below to see the graph!')
+                    except:
+                        st.success("AI Response: Oops! I can't understand, could you elaborate more?")
+
+                else:
+                    # Run chat_with_csv function
+                    #result = get_gpt_response(input_text, chat)
+                    result = pandas_ai.run(data, prompt=input_text)
+                    st.success("AI Response: "+result)
+    if fig is not None:
+        with st.expander('Expand to see the graph'):
+            st.plotly_chart(fig, use_container_width=True)
+
+
+                
--- utils.py
+++ utils.py
@@ -14,6 +14,7 @@ def generate_visual_code(data, prompt, chat):
     sys_msg = SystemMessage(content = 
     f"""
     You are a Python programmer who uses Plotly package to visualize data. You have the following data in the python pandas format: {data}. 
+    assume that the data has been loaded by Pandas package with the variable name "data".
     You will write plot functions like:
     import plotly
         ...
@@ -25,7 +26,7 @@ def generate_visual_code(data, prompt, chat):
     """ 
     human_msg = HumanMessage(content = prompt_template)
     res = chat([sys_msg, human_msg])
-    return res
+    return res.content
 
 def extract_python_code(text):
     index1 = text.find('```python\n')
@@ -33,4 +34,30 @@ def extract_python_code(text):
     code = text[index1+len('```python\n'):index1+index2]
     return code
 
+def get_gpt_response(prompt, chat):
+    human_msg = HumanMessage(content = prompt)
+    response = chat([human_msg])
+    return response.content
 
+def detect_qa_or_plot(prompt, chat):
+    
+    prompt_template = """
+        The following kinds of questions are related to Plotting:
+        - Create a line plot for ...
+        - Plot ....
+        - Generate a graph ...
+        
+        The following kinds of questions are related to Q&A:
+        - Do an analysis for ...
+        - How is ....
+        - Why the stocks are going down?
+        
+        """+f"""
+    '{prompt}'
+    Is this a task for "Plotting" or "Q&A".
+    Reply in the format:
+    category: 
+    """
+    human_msg = HumanMessage(content = prompt_template)
+    res = chat([human_msg])
+    return res.content.split(' ')[-1]
